25c85b76545a5a9739769963b9f451c92d679a48
nhanhas <mpereira132@hotmail.com>
2018-02-11 15:50:11 +0000

    S#03 - nav bar search

---
+++ script.js
@@ -0,0 +1,9 @@
+//check this 
+//https://codepen.io/-J0hn-/pen/vxPPwB?limit=all&page=5&q=e+commerce
+
+//backgroun cool
+/**
+ * 
+ * background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.3)), url("https://static.pexels.com/photos/4827/nature-forest-trees-fog.jpeg") no-repeat center center;
+ * 
+ */
